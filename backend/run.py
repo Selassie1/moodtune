eff68214380ede1a7bced391f8a96c15bb2019dc
scar832 <sedemboafo@gmail.com>
2025-06-06 16:07:37 +0000

    deploying

--- backend/run.py
+++ backend/run.py
@@ -2,8 +2,10 @@ from app import create_app
 from app.extensions import db
 
 app = create_app()
+@app.route("/")
+def index():
+    return "Moodtune backend is running!"
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, port=5001)
+
+with app.app_context():
+    db.create_all()
